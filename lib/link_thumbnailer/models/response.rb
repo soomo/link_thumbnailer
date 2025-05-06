13902478f15f839a02d7619393ff6c6a64c73749
Mark McCraw <mark.mccraw@soomolearning.com>
2025-05-06 14:49:42 -0400

    Add a model to capture relevant response data

---
+++ lib/link_thumbnailer/models/response.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require 'link_thumbnailer/model'
+
+module LinkThumbnailer
+  module Models
+    class Response < ::LinkThumbnailer::Model
+
+      module Types
+        WEBPAGE = 'webpage'
+        IMAGE = 'image'
+        OTHER = 'other'
+      end
+
+      attr_reader :code, :headers, :body
+
+      def initialize(http_response)
+        @code = http_response.code
+        @headers = http_response.header.to_hash
+        @body = http_response.body
+      end
+
+      def content_type
+        (headers['content-type'] || @headers['Content-Type']).first
+      end
+
+      def type
+        case content_type
+        when /^text\/html/, /^application\/html/
+          Types::WEBPAGE
+        when /^image\//
+          Types::IMAGE
+        else
+          Types::OTHER
+        end
+      end
+
+    end
+  end
+end
